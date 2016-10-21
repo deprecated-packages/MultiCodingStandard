ed3ebc41881f21fa812d3f06fe01cad9b8b9b821
TomasVotruba <tomas.vot@gmail.com>
2016-10-21 10:49:45 +0200

    add strict types check

--- tests/ContainerFactory.php
+++ tests/ContainerFactory.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Symplify\MultiCodingStandard\Tests;
 
 use Nette\Configurator;
--- tests/PhpCsFixer/Factory/FixerFactoryTest.php
+++ tests/PhpCsFixer/Factory/FixerFactoryTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Symplify\MultiCodingStandard\Tests\PhpCsFixer\Factory;
 
 use PHPUnit\Framework\TestCase;
